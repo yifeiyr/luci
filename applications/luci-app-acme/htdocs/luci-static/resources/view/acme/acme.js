6b4530dc04863b0a72a92f4503bf6be10090197a
Sergey Ponomarev <stokito@gmail.com>
2024-06-04 16:52:19 +0300

    luci-app-acme: remove the ZeroSSL mention
    Signed-off-by: Sergey Ponomarev <stokito@gmail.com>

--- applications/luci-app-acme/htdocs/luci-static/resources/view/acme/acme.js
+++ applications/luci-app-acme/htdocs/luci-static/resources/view/acme/acme.js
@@ -40,9 +40,6 @@ return view.extend({
 				"point at the router's public IP address. " +
 				"Once configured, issuing certificates can take a while. " +
 				"Check the logs for progress and any errors.") + '<br />' +
-				_('You can sign certificates for free with:') + '<br />' +
-				'<a href="https://LetsEncrypt.org/" target="_blank">LetsEncrypt</a> ' + _('Default') + '<br />' +
-				'<a href="https://ZeroSSL.com/" target="_blank">ZeroSSL</a> ' + '<br />' +
 				_("Cert files are stored in") + ' <em>/etc/ssl/acme</em>' + '<br />' +
 				'<a href="https://openwrt.org/docs/guide-user/services/tls/acmesh" target="_blank">' + _('See more') + '</a>'
 		);
